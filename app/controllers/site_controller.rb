aecb00679a7a66f6b9076c6dd8a0067cb5585bf2
Avi Charlop <avicharlop@gmail.com>
2016-04-18 13:14:01 -0400

    added calculator route to show posting and say_name route to show param use

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,22 @@
 class SiteController < ApplicationController
 	def home
-		# render "home"
+		render "home"
 	end
 	def contact
+		render "contact"
+	end
+	def say_name
+		@name = params[:name]
+		render "say_name"
+	end
+	def calculator
+		render "calculator"
+	end
+	def calculate
+		@first = params[:first_num].to_f
+		@second = params[:second_num].to_f
+		@answer = @first + @second
+		@submited = params[:submit]
+		render "calculator"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 # my added stuff
 get "/", to: "site#home"
 get "/contact", to: "site#contact"
+get "/say_name/:name", to: "site#say_name"
+
+get "/calculator", to: "site#calculator"
+post "/calculator", to: "site#calculate"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
