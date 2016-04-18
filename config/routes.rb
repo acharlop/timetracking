406ef1fd84ed21005e710e9b85c2fd916a9317c6
Avi Charlop <avicharlop@gmail.com>
2016-04-18 11:17:43 -0400

    Index and contact Added route, controllers, action, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+# my added stuff
+get "/", to: "site#home"
+get "/contact", to: "site#contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
